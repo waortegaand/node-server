39bd8f9a99349318bbf6b144b8f78a9d4c4560e8
waortegaand <waortegaand@gmail.com>
2023-04-02 19:11:25 -0500

    Async - await

--- routes/products.router.js
+++ routes/products.router.js
@@ -5,33 +5,40 @@ const ProductsService = require('./../services/product.service')
 const router = express.Router();
 const service = new ProductsService();
 
-router.get("/",(req,res)=>{
-    const productos = service.find();
+router.get("/",async (req,res)=>{
+    const productos = await service.find();
     res.json(productos);
 })
 
-router.get("/:id",(req,res)=>{
+router.get("/:id", async (req,res)=>{
     const {id} = req.params;
-    const producto = service.findOne(id);
+    const producto = await service.findOne(id);
     res.json(producto);
 })
 
-router.post('/',(req,res)=>{
+router.post('/', async (req,res)=>{
     const body = req.body;
-    const newProduct = service.create(body);
+    const newProduct = await service.create(body);
     res.status(201).json(newProduct);
 })
 
-router.patch('/:id',(req,res)=>{
-    const {id} = req.params;
-    const body = req.body;
-    const product = service.update(id,body);
-    res.json(product);
+router.patch('/:id', async (req,res)=>{
+    try {
+        const {id} = req.params;
+        const body = req.body;
+        const product = await service.update(id,body);
+        res.json(product);
+    }catch(error){
+        res.status(404).json({
+            //message:console.error(res)            
+            message : error.message
+        })
+    }
 })
 
-router.delete('/:id',(req,res)=>{
+router.delete('/:id', async (req,res)=>{
     const {id} = req.params;
-    const rta = service.delete(id,body);
+    const rta = await service.delete(id,body);
     res.json(rta);
 })
 
--- services/product.service.js
+++ services/product.service.js
@@ -6,7 +6,7 @@ class ProductsService{
         this.generate();
     }
 
-    generate(){
+    async generate(){
         const limit = 5;
         for (let index=0; index<limit; index++){
             this.products.push({
@@ -18,7 +18,7 @@ class ProductsService{
         }
     }
 
-    create(data){
+    async create(data){
         const newProduct = {
             id: faker.datatype.uuid(),
             ...data
@@ -26,16 +26,20 @@ class ProductsService{
         this.products.push(newProduct);
         return newProduct;
     }
-
+    // Simulacion de retardo de 5 ms
     find(){
-        return this.products;
+        return new Promise((resolve,reject)=>{
+            setTimeout(()=>{
+                resolve(this.products);
+            },5000);
+        })
     }
 
-    findOne(id){
+    async findOne(id){
         return this.products.find(item=>item.id==id);
     }
 
-    update(id, changes){
+    async update(id, changes){
         const index = this.products.findIndex(item=>item.id==id);
         if(index==-1){
             throw new Error('Product not found');
@@ -48,7 +52,7 @@ class ProductsService{
         return this.products[index];
     }
 
-    delete(id){
+    async delete(id){
         const index = this.products.findIndex(item=>item.id==id);
         if(index==-1){
             throw new Error('Product not found');
